b59535f13f291292f709ad722e20115bf432713b
Nicole <nicoleyu@outlook.sg>
2021-05-26 15:08:31 +0800

    Initial Commit

---
+++ NDDEyeTracking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NDDEyeTracking
+//
+//  Created by Nicole Yu on 26/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
